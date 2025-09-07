a734fff8f2d031eac622dd7d2a4c5bd678db27c3
choijieun <choijieun@hanwha.com>
2025-09-07 15:48:52 +0900

    search input component

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="relative">
+    <input
+      type="text"
+      :placeholder="placeholder"
+      class="w-96 h-11 px-4 py-1.5 pl-12 bg-background-neutral border-line-neutral border-solid inline-flex border rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder:text-gray-400 placeholder:text-base placeholder:font-normal placeholder:leading-tight"
+      :style="inputStyle"
+      v-model="modelValue"
+      @input="$emit('update:modelValue', modelValue)"
+    />
+    <img
+      src="@/assets/icons/search-icon.svg"
+      alt="검색"
+      class="absolute left-4 top-1/2 transform -translate-y-1/2 w-5 h-5"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: "",
+  },
+  placeholder: {
+    type: String,
+    default: "",
+  },
+  inputStyle: {
+    type: [String, Object],
+  },
+});
+const modelValue = ref(props.modelValue);
+</script>
--- src/plugins/global-components.js
+++ src/plugins/global-components.js
@@ -1,9 +1,11 @@
 import BaseButton from "@/components/BaseButton.vue";
 import ContextMenu from "@/components/ContextMenu.vue";
+import SearchInput from "@/components/SearchInput.vue";
 
 export default {
   install(app) {
     app.component("BaseButton", BaseButton);
     app.component("ContextMenu", ContextMenu);
+    app.component("SearchInput", SearchInput);
   },
 };
