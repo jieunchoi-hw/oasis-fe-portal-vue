51bdbf8a08d2ab9bb7f6b10770df92d81c264feb
choijieun <choijieun@hanwha.com>
2025-09-07 13:43:46 +0900

    app header router link 조절

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -26,7 +26,7 @@
           to="/document-storage"
           class="px-3 py-2 text-base font-semibold transition-colors h-9 flex items-center"
           :class="
-            $route.path === '/document-storage'
+            $route.path.startsWith('/document-storage')
               ? 'text-neutral-900'
               : 'text-neutral-500 hover:text-neutral-700'
           "
